86936b3c8d835f5bf82c7c46e1006fd8ded43b7c
KECO-00 <ksang98@icloud.com>
2023-01-24 15:28:18 +0900

    week2

---
+++ week2/src/week2/BOJ_1946.java
@@ -0,0 +1,39 @@
+package week2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Comparator;
+public class BOJ_1946 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for(int tc = 1 ; tc <= T ; tc++ ) {
+			int N = sc.nextInt();
+			int[] arr = new int[N+1];
+			int result = 1;
+			
+			for(int i = 0 ; i < N ; i++) {
+				int paper = sc.nextInt();
+				int interview = sc.nextInt();
+				arr[paper] = interview;
+			}
+			
+			int standard = arr[1];
+			for(int i = 2; i<=N ; i++) {
+				if(standard > arr[i]) {
+					result++;
+					standard = arr[i];
+				}
+			}
+			
+			
+			System.out.println(result);
+		}
+		
+		sc.close();
+	}
+
+}
