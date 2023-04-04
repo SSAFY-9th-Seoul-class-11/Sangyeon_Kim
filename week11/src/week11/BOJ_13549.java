97935df6dd6a70fb11ca6fbf24c57ff15b96e4a1
SangYeon Kim <64177079+KECO-00@users.noreply.github.com>
2023-04-04 13:07:26 +0900

    Update BOJ_13549.java

--- week11/src/week11/BOJ_13549.java
+++ week11/src/week11/BOJ_13549.java
@@ -22,9 +22,9 @@ public class BOJ_13549 {
 		visited[n] = 1;
 		
 		
-		if(n > k) {
+		if(n > k) {// 동생이 수빈이보다 뒤에 있는 경우 
 			System.out.println(n - k);
-		}else if(n==k) {
+		}else if(n==k) { // 동생의 위치가 수빈이랑 같은 경우
 			System.out.println(0);
 		}else {
 			bfs();			
@@ -35,8 +35,8 @@ public class BOJ_13549 {
 
 	public static void bfs() {
 		Queue<Integer> queue = new ArrayDeque<>();
-		queue.add(n);
-		int[] cnt = new int[MAX];
+		queue.add(n);	//현재 위치를 큐에 추가
+		int[] cnt = new int[MAX]; //이동 시간을 저장할 배열 cnt를 생성
 		
 		while(!queue.isEmpty()) {
 			int x = queue.poll();
@@ -46,22 +46,22 @@ public class BOJ_13549 {
 			}
 			for(int i = 0 ; i < 3 ; i++) {
 				int nx;
-				if(i==0) {
+				if(i==0) { //2배 이동할때
 					nx = x * 2;
-				}else if(i == 1) {
+				}else if(i == 1) {	// -1 이동할 때
 					nx = x - 1;
-				}else {
+				}else {	// +1 이동할 때
 					nx = x + 1;
 				}
 				
-				if(nx >= 0 && nx <MAX && visited[nx] == 0) {
-					queue.add(nx);
+				if(nx >= 0 && nx <MAX && visited[nx] == 0) {	// 범위 내에 있고, 방문 안했으면
+					queue.add(nx);	// 해당 위치를 큐에 추가
 					
-					visited[nx] = 1;
-					if(i != 0) {
-						cnt[nx] = cnt[x] + 1;
-					}else {
-						cnt[nx] = cnt[x];
+					visited[nx] = 1; //방문 표시
+					if(i != 0) {	// 2배 이동하는게 아닌 경우
+						cnt[nx] = cnt[x] + 1;	// 이동 시간을 +1 해줌
+					}else {	// 2배 이동하는 경우에는
+						cnt[nx] = cnt[x];	// 이동시간 그대로
 					}
 				}
 			}
